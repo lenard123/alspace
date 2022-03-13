c3946d0947280d413c86c0c0f2681c42b160597b
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-03-13 10:11:41 +0000

    fix image links

--- front-end/src/pages/profile/Profile.jsx
+++ front-end/src/pages/profile/Profile.jsx
@@ -1,12 +1,13 @@
 import './profile.css'
 import Avatar from '/src/components/avatar'
 import Feed from '/src/components/feed'
+import Cover from '/assets/cover.png'
 
 export default function Profile() {
     return (
         <div>
             <div className="cover">
-                <img className="cover-img" src="/assets/cover.png"/>
+                <img className="cover-img" src={Cover}/>
                 <Avatar className="profile-picture" size="120px" src="https://avatars.dicebear.com/api/initials/Lenard+Mangay-ayam.svg" />
             </div>
 
