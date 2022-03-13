f9adefa690b0bf9a8363749d8bf4114f742d6e02
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-03-13 11:18:34 +0000

    update

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import { MenuOutlined } from '@ant-design/icons'
 
 export default () => (
     <div className="bg-gray-100 min-h-screen text-gray-700">
-        <header className="shadow bg-white md:bg-red-500 h-16 px-4 flex items-center justify-between">
+        <header className="shadow-lg bg-white md:bg-red-500 h-16 px-4 flex items-center justify-between">
             <div className="flex items-center gap-2">
                 <img className="rounded" src={Logo} height="32" width="auto" />
                 <span className="font-bold text-xl">Alspace</span>
