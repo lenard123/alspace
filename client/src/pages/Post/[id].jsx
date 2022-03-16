a194e8a7815abb0a871d17694ccc82bd04862779
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-03-16 11:26:14 +0000

    Add Comment form

--- client/src/pages/Post/[id].jsx
+++ client/src/pages/Post/[id].jsx
@@ -1,14 +1,27 @@
+import { Avatar, Comment, Input, Button } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
 import Post from '/src/components/Post/Post'
-import Comment from '/src/components/Comment/Comment'
+import PostComment from '/src/components/Comment/Comment'
 
 export default function()
 {
     return (
         <div className='my-4 max-w-xl mx-auto'>
             <Post>
-                <Comment />
-                <Comment />
-                <Comment />
+                <PostComment />
+                <PostComment />
+                <PostComment />
+
+                <Comment 
+                    avatar={<Avatar icon={<UserOutlined />}/>}
+                    content={
+                        <div className='space-y-2'>
+                            <Input.TextArea rows={2} placeholder='Write a comment' />
+                            <Button type='primary'>Submit</Button>
+                        </div>
+                    }
+                />
+
             </Post>
         </div>
     )
