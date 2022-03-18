08128263d7f50850a0cc41b73cc5e52e2d17b9fe
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-03-19 02:35:35 +0800

    delete client folder

--- client/src/components/icons/iconator.jsx
+++
@@ -1,7 +0,0 @@
-import Icon from '@ant-design/icons'
-
-export default (svgIcon, props) => {
-    const _icon = () => svgIcon
-
-    return <Icon component={_icon} {...props} />
-}
--- client/src/hooks/index.js
+++
@@ -1,6 +0,0 @@
-
-export const sleep = (ms) => new Promise(resolve => {
-    setTimeout(resolve, ms)
-})
-
-export { default as useToggler } from './useToggler'
