9a134e8cccca9fc090b48294c928758966dbb4ef
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-03-18 22:12:07 +0800

    Delete front-end folder

--- front-end/src/components/shadow/Shadow.jsx
+++
@@ -1,9 +0,0 @@
-import './shadow.css'
-
-export default function Shadow({className, children}) {
-    return (
-        <div className={`custom-shadow ${className}`}>
-            {children}
-        </div>
-    )
-}
--- front-end/src/pages/job-detail/JobDetail.jsx
+++
@@ -1,9 +0,0 @@
-export default function JobDetail() {
-    return (
-        <div className="grid grid-cols-9">
-            <div className="col-span-5 p-5">
-                test
-            </div>
-        </div>
-    )
-}
