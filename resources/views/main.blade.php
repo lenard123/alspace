bcb0ac72ab7abca178c9d181bd98168a11f723c1
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-03-24 04:08:12 +0000

    Fix Configurations

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/src/favicon.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Laravel + Vite App</title>
-  </head>
-  <body class="font-sans">
-    <div id="root"></div>
 
-    @if(config('app.env') === 'production')
-      @vite
-    @else
-      <script type="module">
-        import RefreshRuntime from "{{ env('VITE_SERVER_HOST', 'http://localhost:3000') }}/@@react-refresh"
-        RefreshRuntime.injectIntoGlobalHook(window)
-        window.$RefreshReg$ = () => {}
-        window.$RefreshSig$ = () => (type) => type
-        window.__vite_plugin_react_preamble_installed__ = true
-      </script>
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="/src/favicon.svg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Laravel + Vite App</title>
+</head>
 
-      <script type="module" src="{{ env('VITE_SERVER_HOST', 'http://localhost:3000') }}/@@vite/client"></script>
-      <script type="module" src="{{ env('VITE_SERVER_HOST', 'http://localhost:3000') }}/resources/js/main.jsx"></script>
-    @endif
+<body class="font-sans">
+  <div id="root"></div>
 
-  </body>
+  @if(config('app.env') === 'gitpod')
+  <script type="module">
+    import RefreshRuntime from "{{ env('VITE_SERVER_HOST', 'http://localhost:3000') }}/@@react-refresh"
+    RefreshRuntime.injectIntoGlobalHook(window)
+    window.$RefreshReg$ = () => {}
+    window.$RefreshSig$ = () => (type) => type
+    window.__vite_plugin_react_preamble_installed__ = true
+  </script>
+
+  <script type="module" src="{{ env('VITE_SERVER_HOST', 'http://localhost:3000') }}/@@vite/client"></script>
+  <script type="module" src="{{ env('VITE_SERVER_HOST', 'http://localhost:3000') }}/resources/js/main.jsx"></script>
+  @else
+    @vite
+  @endif
+
+</body>
+
 </html>
