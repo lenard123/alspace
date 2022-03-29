d31c444aeeb54b1adb00784e159f1cb0fea57c84
lenard456 <light.yagami0727@gmail.com>
2022-03-29 10:01:46 +0000

    Remove Sidebar

--- resources/js/components/layout/MainLayout.jsx
+++ resources/js/components/layout/MainLayout.jsx
@@ -1,21 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Topbar from '@/js/components/layout/Topbar'
-import Sidebar from '@/js/components/layout/Sidebar'
 
-export default ({children}) => (
+const MainLayout = () => (
     <div className='bg-gray-50 min-h-screen text-gray-700 flex flex-col'>
 
         <Topbar />
         
-        <div className="flex-grow grid grid-cols-3 lg:grid-cols-4 gap-2 sm:mx-4">
-
-            <Sidebar />
-
-            <main className='lg:border-l border-gray-300 col-span-3'>
-                <Outlet />
-            </main>
-
+        <div>
+            <Outlet />
         </div>
 
     </div>
 )
+
+export default MainLayout
