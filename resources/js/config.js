452ab7fe8e17c991f73b4b554b4221cc15ad656c
Lenard  Mangay-ayam <31153286+lenard123@users.noreply.github.com>
2022-03-30 18:36:20 +0800

    Fix Configuration

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,7 +1,9 @@
 const isGitpod = !!import.meta.env.VITE_ISGITPOD
 
 const CONFIGURATION = Object.freeze({
-    'API_URL': isGitpod ? `https://${import.meta.env.VITE_SERVER_HOST}/api` : 'http://localhost:8000/api'
+    'API_URL': isGitpod 
+        ? `https://${import.meta.env.VITE_SERVER_HOST}/api` 
+        : import.meta.env.VITE_API_URL || '/api'
 })
 
 const config = (key, fallback = null) => {
