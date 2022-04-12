2cfcd4f36262648d10ebb54b57746edc08d6d471
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2022-04-12 17:11:21 +0000

    WIP

--- resources/js/pages/events/components/EventCardAction/EventCardAction.jsx
+++ resources/js/pages/events/components/EventCardAction/EventCardAction.jsx
@@ -3,28 +3,33 @@ import { useContext } from 'react'
 import { EventContext } from "../EventCard";
 import { Spin } from "antd";
 import { useMutation } from "react-query";
-import { cancelParticipation } from "@/js/apis/EventApi";
+import { addToInterested, cancelParticipation } from "@/js/apis/EventApi";
 import { LoadingOutlined } from "@ant-design/icons";
 import useEventMutator from "@/js/queries/useEventMutator";
+import classNames from "classnames";
 
 export default function EventCardAction() {
     const { is_interested, id } = useContext(EventContext)
     const { updateEvent } = useEventMutator()
-    const { isLoading, mutate } = useMutation(cancelParticipation, {
+    const { isLoading, mutate } = useMutation(
+        () => is_interested 
+            ? cancelParticipation(id) 
+            : addToInterested(id), {
         onSuccess: (data) => {
             updateEvent(data)
         }
     })
 
-    if (is_interested) {
-        return (
-            <Spin spinning={isLoading} indicator={<LoadingOutlined />}>
-                <button onClick={() => mutate(id)} className='w-full rounded text-sm btn btn-secondary active'>
-                    <span className='font-semibold'>Interested</span>
-                </button>
-            </Spin>
-        )
-    }
+    return (
+        <Spin spinning={isLoading} indicator={<LoadingOutlined />}>
+            <button 
+                onClick={() => mutate()} 
+                className={classNames(`w-full rounded text-sm btn btn-secondary`, {'active': is_interested})}
+                >
+                <span className='font-semibold'>Interested</span>
+            </button>
+        </Spin>
+    )
 
     return <InterestedButton />
 }
