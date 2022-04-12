23e493fe0688e5e25bee753c81b5da1dbda15a40
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2022-04-12 09:39:54 +0000

    Install Telescope, Debugbar, and Session Tables

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (env('APP_GITPOD', false)) {
+            URL::forceRootUrl(config('app.url'));
+            if (Str::contains(config('app.url'), 'https://')) {
+                URL::forceScheme('https');
+            }
+        }
+
     }
 }
