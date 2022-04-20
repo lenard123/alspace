17e93fb817ec14502de71e03f2dea8c18b7265fe
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2022-04-20 15:09:17 +0000

    Fix site icon

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8" />
-  <link rel="icon" type="image/svg+xml" href="/src/favicon.svg" />
+  <link rel="icon" type="image/png" href="/images/logo.png" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
   <title>Alspace</title>
   <link rel="manifest" href="/manifest.json">
