f32be17ccffb6a4d5316df903714420a806db6f9
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-05-02 10:18:38 +0000

    Bug fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,6 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \App\Http\Middleware\HttpsProtocol::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
