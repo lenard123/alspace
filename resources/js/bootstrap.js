1d5bfc5894034b560be5b779aa35a8de282153f4
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-05-04 03:09:27 +0000

    Refactor ReactQuery Wrapper

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,6 +2,8 @@ import moment from 'moment'
 import { Http, Echo } from '@/js/utils'
 import Cookie from 'js-cookie'
 import _ from 'lodash'
+import { queryClient } from './query/ReactQueryProvider'
+
 
 //Export common functions to windows
 window._ = _
@@ -9,3 +11,4 @@ window.moment = moment
 window.Http = Http
 window.Cookie = Cookie
 window.Echo = Echo
+window.queryClient = queryClient
