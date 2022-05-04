c14bab7f986ebc1766bae241f883cda58cb22d6d
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-05-04 03:19:20 +0000

    Remove comments

--- resources/js/query/queries/usePendingUsersQuery.js
+++ resources/js/query/queries/usePendingUsersQuery.js
@@ -12,19 +12,4 @@ export default function usePendingUsersQuery(page = 1)
             return data.data
         }
     })
-    // return useInfiniteQuery({
-    //     queryKey: queryKeyFactory.pendingUsers,
-    //     queryFn: ({ pageParam }) => UserApi.fetchPendingUsers(pageParam),
-    //     select: ({ pages }) => {
-    //         return pages.map(page => {
-    //             return page.data
-    //         }).flat()
-    //     },
-    //     getNextPageParam: (lastPage) => {
-    //         if (lastPage.current_page < lastPage.last_page) {
-    //             return lastPage.current_page + 1
-    //         }
-    //         return undefined
-    //     }
-    // })
 }
