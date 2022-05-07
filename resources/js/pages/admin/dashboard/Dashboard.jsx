5f25b7fabbecf8ee7e5ae9019b776f4db5c3388a
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-05-07 09:00:40 +0000

    Display Cards

--- resources/js/pages/admin/dashboard/Dashboard.jsx
+++ resources/js/pages/admin/dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
-import { Breadcrumb, PageHeader } from "antd";
+import { CalendarTwoTone, IdcardOutlined, IdcardTwoTone, MessageOutlined, UserOutlined } from "@ant-design/icons";
+import { Breadcrumb, Card, PageHeader, Statistic } from "antd";
 import BreadcrumbItem from "antd/lib/breadcrumb/BreadcrumbItem";
 
 export default function Dashboard() {
@@ -11,6 +12,42 @@ export default function Dashboard() {
                     </Breadcrumb>
                 }
             />
+
+            <div className='mx-6'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+                    <Card>
+                        <Statistic
+                            title='Pending Registration'
+                            value={100}
+                            prefix={<UserOutlined className='text-green-500' />}
+                        />
+                    </Card>
+
+                    <Card>
+                        <Statistic
+                            title='Upcoming Events'
+                            value={100}
+                            prefix={<CalendarTwoTone />}
+                        />
+                    </Card>
+
+                    <Card>
+                        <Statistic
+                            title='Item Requests'
+                            value={100}
+                            prefix={<IdcardOutlined className='text-pink-500' />}
+                        />
+                    </Card>
+
+                    <Card>
+                        <Statistic
+                            title='Unread Messages'
+                            value={100}
+                            prefix={<MessageOutlined className='text-orange-500' />}
+                        />
+                    </Card>
+                </div>
+            </div>
         </>
     )
 }
