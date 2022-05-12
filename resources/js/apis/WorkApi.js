4fd80f1c43385c754228eb88810089eac6b1ae6c
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-05-12 09:42:05 +0000

    Bug Fix

--- resources/js/apis/WorkApi.js
+++ resources/js/apis/WorkApi.js
@@ -1,6 +1,11 @@
+import moment from "moment"
 import Http, { requestCookie } from "../utils/Http"
 
 export const addWorkExperience = async (data) => {
     await requestCookie()
-    return await Http.post('/works', data)
+    return await Http.post('/works', {
+        ...data,
+        start_at: data.start_at.format('YYYY-MM-DD'),
+        end_at: data.end_at?.format('YYYY-MM-DD')
+    })
 }
