8cf3aa7d9626d216e63ec1e86a04ca793015e784
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-05-13 07:47:13 +0000

    Add Payload  Column on Images Table

--- app/Models/Concerns/HasAvatar.php
+++ app/Models/Concerns/HasAvatar.php
@@ -9,24 +9,15 @@ trait HasAvatar
 {
     public function avatar(): MorphOne
     {
-        return $this->morphOne(Image::class, 'imageable');
+        return $this->morphOne(Image::class, 'imageable')
+            ->where('payload', 'avatar');
     }
 
     public function getAvatarUrlAttribute() : string
     {
-        return $this->avatar->url;
-    }
-
-    public function regenerateAvatar() : void
-    {
-        //Delete if has avatar
-        $this->avatar()->delete();
-
         //Generate new
         $name = strtolower(urlencode($this->fullname));
-        $this->avatar()->create([
-            'source' => 'url',
-            'reference' => "https://avatars.dicebear.com/api/initials/$name.svg"
-        ]);
+
+        return $this->avatar?->url ?? "https://avatars.dicebear.com/api/initials/$name.svg";
     }
 }
--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -6,15 +6,5 @@ use App\Models\User;
 
 class UserObserver
 {
-    /**
-     * Handle the User "created" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function created(User $user)
-    {
-        $user->regenerateAvatar();
-    }
 
 }
