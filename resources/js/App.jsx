cb9ab1a8ffe930990e3f3538b24eb9b7bf576e7e
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-05-23 05:11:44 +0000

    Remove Recoil

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,13 +1,10 @@
 import Router from "./router/Router"
-import { RecoilRoot } from "recoil";
 import ReactQueryProvider from "./query/ReactQueryProvider";
 
 
 const App = () => (
   <ReactQueryProvider>
-    <RecoilRoot>
-      <Router />
-    </RecoilRoot>
+    <Router />
   </ReactQueryProvider>
 )
 
