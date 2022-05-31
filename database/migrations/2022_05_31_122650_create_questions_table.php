2911874b0a89ea442107ff4bbfac12de46404301
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-05-31 16:05:05 +0000

    Change data type of answer from string to text

--- database/migrations/2022_05_31_122650_create_questions_table.php
+++ database/migrations/2022_05_31_122650_create_questions_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('question');
-            $table->string('answer');
+            $table->text('answer');
         });
     }
 
