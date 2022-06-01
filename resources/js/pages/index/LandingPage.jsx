37cfd76468e3ec2c9a4292422acfc75a1f296ee2
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-06-01 09:47:44 +0000

    Landing Page redesign

--- resources/js/pages/index/LandingPage.jsx
+++ resources/js/pages/index/LandingPage.jsx
@@ -1,9 +1,12 @@
+import Footer from "@/js/components/Footer/Footer";
 import Topbar from "@/js/components/Topbar/Topbar";
 import { useCurrentUser } from "@/js/query/queries/useCurrentUserQuery";
-import { Carousel } from "antd";
+import { Button, Carousel, Image } from "antd";
 import Helmet from 'react-helmet'
+import { Link } from "react-router-dom";
 import GuestTopbar from "./components/GuestTopbar";
 
+const image = 'https://export-download.canva.com/c625b8db-2ec0-4c4b-bf03-d656640b00af/0/0001-27580224269.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20220531%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220531T121750Z&X-Amz-Expires=81414&X-Amz-Signature=b1ca4b4230151d98f20d526d53c4c6bc1343cbe350da6180410655365fdd6b87&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Welcome%2520to%2520the%2520club.png&response-expires=Wed%2C%2001%20Jun%202022%2010%3A54%3A44%20GMT'
 
 const contentStyle = {
     height: 'min(300px, 60vw)',
@@ -11,6 +14,9 @@ const contentStyle = {
     placeItems: 'center',
     color: '#fff',
     background: '#364d79',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundImage: `url(${image})`,
 };
 
 const features = [
@@ -69,22 +75,25 @@ export default function LandingPage() {
                 <title>Alspace - A place for UCC CSD Alumni</title>
             </Helmet>
 
-            {isLoggedIn ? <Topbar/> : <GuestTopbar />}
-            <Carousel>
-                <div>
-                    <h3 style={contentStyle}>1</h3>
+            {isLoggedIn ? <Topbar /> : <GuestTopbar />}
+
+            <div className="text-center py-16">
+                <h1 className="font-black text-4xl">Alumni Manage System</h1>
+                <p>A place where for CSD Alumni</p>
+                {!isLoggedIn && <Link to='/register'><Button type='primary' shape='round' size='large'>Sign up now</Button></Link> }
+
+                <div className='page-wrapper mt-8'>
+                    <Carousel className="max-w-screen-md mx-auto">
+                        <Image className='rounded-lg' src='/images/1.png' />
+                        <Image className='rounded-lg' src='/images/2.png' />
+                        <Image className='rounded-lg' src='/images/3.png' />
+                        <Image className='rounded-lg' src='/images/4.png' />
+                        <Image className='rounded-lg' src='/images/5.png' />
+                    </Carousel>
                 </div>
-                <div>
-                    <h3 style={contentStyle}>2</h3>
-                </div>
-                <div>
-                    <h3 style={contentStyle}>3</h3>
-                </div>
-                <div>
-                    <h3 style={contentStyle}>4</h3>
-                </div>
-            </Carousel>
-            {/* <Alert type='info' message='SIGN UP NOW! BE PART OF OUR COMMUNITY!' banner closable/> */}
+
+            </div>
+
 
             <div className='page-wrapper py-8'>
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
@@ -103,6 +112,7 @@ export default function LandingPage() {
                 </div>
             </div>
 
+            <Footer />
         </div>
     )
 }
